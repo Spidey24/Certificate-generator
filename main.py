928fac1d006a45b1800970dccd3691d6831bd925
Aditya Kshirsagar <75561293+Spidey24@users.noreply.github.com>
2023-03-06 17:25:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def main():
    #print(name_list)
    for i,n in enumerate(name_list):
       template = cv2.imread('certificate-template.jpg')
-      cv2.putText(template,n,(1022,765),cv2.FONT_HERSHEY_SCRIPT_COMPLEX,4.5,(0,0,0,0),2,cv2.LINE_AA)
-      cv2.imwrite(f'D:/Python_learning/3_Projects/Certificate-generator/Generated_Certificate/{n}.jpg',template)
+      cv2.putText(template,n,(1022,765),cv2.FONT_HERSHEY_SCRIPT_COMPLEX,4.5,(0,0,0,0),2,cv2.LINE_AA) 
+      cv2.imwrite(f'Your-path/Generated_Certificate/{n}.jpg',template) #Replace Your-path with your desired path 
       print('Processing Certificate{}/{}'.format(i+1,len(name_list)))
 if __name__ == '__main__':
    main()
